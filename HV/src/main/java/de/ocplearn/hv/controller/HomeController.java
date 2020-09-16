304c0ab239eb4aea77a5828c2e0ccf4ba966baaa
HH-DR <69295577+HH-DR@users.noreply.github.com>
2020-09-16 11:15:25 +0200

    wundervoll

---
+++ HV/src/main/java/de/ocplearn/hv/controller/HomeController.java
@@ -0,0 +1,15 @@
+package de.ocplearn.hv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
