eec39f19787490d98753468b19d0d703010b7cd6
HH-DR <69295577+HH-DR@users.noreply.github.com>
2020-09-16 10:41:21 +0200

    HUHU

---
+++ HV/src/main/java/de/ocplearn/hv/HvApplication.java
@@ -0,0 +1,13 @@
+package de.ocplearn.hv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HvApplication.class, args);
+	}
+
+}
---
+++ HV/src/test/java/de/ocplearn/hv/HvApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ocplearn.hv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
