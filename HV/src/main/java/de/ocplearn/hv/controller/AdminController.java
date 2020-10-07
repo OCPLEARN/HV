7ccf184b8be0c96a7c221e3be695c8f15f053a4b
HH-DR <69295577+HH-DR@users.noreply.github.com>
2020-10-07 17:13:52 +0200

    Delete fertig Update halbfertig

--- HV/src/main/java/de/ocplearn/hv/controller/AdminController.java
+++ HV/src/main/java/de/ocplearn/hv/controller/AdminController.java
@@ -5,7 +5,9 @@ package de.ocplearn.hv.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,6 +35,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.context.WebContext;
 
+import de.ocplearn.hv.dao.LoginUserDao;
 import de.ocplearn.hv.dto.LoginUserDto;
 import de.ocplearn.hv.model.Role;
 import de.ocplearn.hv.service.UserService;
@@ -86,11 +89,54 @@ public class AdminController {
 		return "/admin/page3";
 	}
 	
-	@GetMapping("/users/delete/{loginusername}")
-	public String deleteUserFromList( ) {
+	@GetMapping("/users/delete")
+	public String deleteUserFromList( @RequestParam("loginusername") String loginUserName) {
 		
-		return "redirect:/users?delete=succes";
+		String msg;
+//		Infos und Warnmeldungen für die Infoschaltfläche in der Navbar	
+//		msg = "msg=neutrale Info";
+//		msg = "msgWarn=Warnung";
+		
+		if (userService.deleteUser(loginUserName)) {
+			msg = "msgSuccess=User gelöscht!";
+		} else {
+			msg = "msgFail=User konnte nicht gelöscht werden.";
+		}
+		return "redirect:/admin/users?" + msg; 
 	}
+	
+	@GetMapping("/users/edit")
+	public String editUserFromList( @RequestParam("loginusername") String loginUserName, Model model) {
+		
+		LoginUserDto loginUserDto = userService.findUserByLoginUserName(loginUserName);
+		model.addAttribute("loginUser", loginUserDto); //Definition of variable for Thymeleaf
+		
+		
+		return "/admin/usersedit";
+		
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUserFromList(@ModelAttribute("loginUser") LoginUserDto loginUserDto) {
+		
+		System.out.println(loginUserDto.getRole());
+		
+		
+		String msg;
+//		Infos und Warnmeldungen für die Infoschaltfläche in der Navbar	
+//		msg = "msg=neutrale Info";
+//		msg = "msgWarn=Warnung";
+		
+		if (userService.updateUser(loginUserDto)) {
+			msg = "msgSuccess=User wurde aktualisiert!";
+		} else {
+			msg = "msgFail=User konnte nicht aktualisiert werden.";
+		}
+		return "redirect:/admin/users?" + msg; 
+		
+	
+	}
+	
 	
 	
 }
