57f679ffaf32cd62956bdcabae2ad555158d167e
DESKTOP-KVB96NE\julian <julian.nadarz@gmail.com>
2020-11-03 14:44:29 +0100

    Building + basic class constructs

--- HV/src/main/java/de/ocplearn/hv/model/Unit.java
+++ HV/src/main/java/de/ocplearn/hv/model/Unit.java
@@ -4,4 +4,29 @@ public class Unit {
 	
 	private int id;
 
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Unit(int id) {
+		super();
+		this.id = id;
+	}
+
+	public Unit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
 }
