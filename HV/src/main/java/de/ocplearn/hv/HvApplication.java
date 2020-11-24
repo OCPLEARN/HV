d196260777d82c3f86a99059825e16280ff350d9
HH-DR <69295577+HH-DR@users.noreply.github.com>
2020-11-24 12:13:23 +0100

    Check Storage Container on Start Application

--- HV/src/main/java/de/ocplearn/hv/HvApplication.java
+++ HV/src/main/java/de/ocplearn/hv/HvApplication.java
@@ -1,21 +1,41 @@
 package de.ocplearn.hv;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class HvApplication {
-
+	
+	private static String environmentVariable;
+	private static String userVariable;
+	private static String storageEntryPoint = "immodata";
+	public static final String storageEntryPointAbsolutePath;
+	
+	static {
+		
+		if( File.separatorChar == '/') {
+			userVariable = "USER";
+			environmentVariable = "HOME";
+			
+		}else if ( File.separatorChar == '\\' ){
+			userVariable = "USERDOMAIN";
+			environmentVariable = "USERPROFILE";
+		}else {
+			throw new IllegalStateException("Environment is neither MACOSX nor Windows. Buy a new computer!");
+		}		
+		// Get the entry point to data storage
+		File storageContainer = new File( System.getenv(environmentVariable) + File.separatorChar + storageEntryPoint );
+		storageEntryPointAbsolutePath = storageContainer.getAbsolutePath();
+		
+		if(!storageContainer.exists()) { 
+			System.exit(1);
+		}	
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(HvApplication.class, args);
-		
-		
+		SpringApplication.run(HvApplication.class, args);		
 	}
 
 
